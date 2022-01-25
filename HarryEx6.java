fb45ae3db67922e8bf4381e1bb3774dd961f7612
Deyvant R_K_Bhardwaj <58858873+DeyvWeb@users.noreply.github.com>
2022-01-25 20:06:38 +0530

    Add files via upload

---
+++ HarryEx6.java
@@ -0,0 +1,182 @@
+// Question 1: Write a program to print the following pattern :
+
+// ****
+
+// ***
+
+// **
+
+// *
+import java.util.Scanner;
+
+public class HarryEx6 {
+    public static void main(String[] args) {
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print(" How many number you want to print? ");
+        // int n = sc.nextInt();
+        
+
+        // for(int i=n; i>0; i--)
+        // {
+        //  for(int j = 0; j<i; j++)
+        //      {
+        //     for(int k = 0; k<j; k++){
+        //         System.out.print("*");
+        //     }
+                
+        //      }
+        //      System.out.print("\n");
+        //  }
+
+// Question 2: Write a program to sum first n even numbers using a while loop.
+
+            
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print(" How many number you want to print even numbers? ");
+//         int n = sc.nextInt();
+// int sum = 0;
+// int i = 0;
+//         while (i<n){
+//                 i++;
+//                 sum = sum + (2*i);
+               
+//         }
+//         System.out.print(sum);
+
+// Question 3: Write a program to print the multiplication table of a given number n.
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print(" Provide a number to check out the table? ");
+        // int n = sc.nextInt();
+
+        // for(int i=1; i<=10; i++ )
+        // {
+            
+        //     int mul = (n*i);
+        //     System.out.println(n+ " X " + i + " = " + mul);
+            
+        // }
+
+// Question 4: Write a program to print a multiplication table of 10 in reverse order.
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print(" Provide a number to check out the table? ");
+        // int n = sc.nextInt();
+
+        // for(int i=10; i>=1; i-- )
+        // {
+            
+        //     int mul = (n*i);
+        //     System.out.println(n+ " X " + i + " = " + mul);
+            
+        // }
+
+// Question 5: Write a program to find the factorial of a given number using for loops.
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print(" Provide a number to find factorial? ");
+        // int n = sc.nextInt();
+        // int fact =1;
+        
+
+        // for(int i=1; i<=n; i++ )
+        // {
+        //     fact = fact*i;
+        // }
+        // System.out.println(fact);
+
+// Question 6: Repeat problem 5 using a while loop.
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print(" Provide a number to find factorial? ");
+        // int n = sc.nextInt();
+        // int i = 1;
+        // int fact = 1;
+
+        // while(i <=n )
+        // {
+        //         fact *= i;
+        //         i++;
+                
+        // }
+        // System.out.print(fact);
+
+// Question 7: Repeat problem 1 using for/while loop.
+
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print(" How many number you want to print? ");
+        // int n = sc.nextInt();
+        // int i = n;
+
+        // while(i>0)
+        // {
+        //         i--;
+        //         int j = 0;
+        //         while(j<i)
+        //         {
+        //                 j++;
+        //                 System.out.print("*");
+                        
+        //         }
+        //         System.out.print("\n");
+        // }
+
+
+        // Question 8: What can be done using one type of loop can also be done using the other two types of loops - True or False.
+        // Yes and No in some cases cause of the requirement set by the user to retrieve the information accordingly. 
+        // We can say it as if we don't know the exact limit of any sort of algorithm we'll be using while loop and if we know the finite value for loop can be valuable aspect to look upon for.
+
+// Question 9: Write a program to calculate the sum of the numbers occurring in the multiplication table of 8.
+
+//        Scanner sc = new Scanner(System.in);
+//         System.out.print(" Provide a number to check out the table? ");
+//         int n = sc.nextInt();
+//         int sum = 0;
+
+//         for(int i=1; i<=10; i++ )
+//         {
+            
+//             int mul = (n*i);
+//             System.out.println(n + " X " + i + " = " + mul);
+            
+//             sum = (sum + mul);   
+//         }
+//         System.out.print(sum);
+
+
+// Question 10 :A do-while loop is executed:
+
+//        At least once - Correct Option
+//        At least twice
+//       At most once
+
+//        Scanner sc = new Scanner(System.in);
+//         System.out.print(" Provide a number to check out the table? ");
+//         int n = sc.nextInt();
+//         int i =0;
+
+//         do{
+//                 System.out.print(i);
+//                 i++;
+
+//         }while(i<n);
+
+
+
+
+// Question 11: Repeat problem 2 using for loop.
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print(" How many number you want to print even numbers? ");
+        // int sum = 0;
+        // int n = sc.nextInt();
+        // for(int i = 0; i<n; i++)
+        // {
+        //     sum = sum + (2*i);
+        // }
+        // System.out.println(sum);
+    }
+}
+
+
+
